ad99d82b65e16ceadd91fa22488027d3750aa480
PankivAndrew <pankiv_a@ucu.edu.ua>
2018-02-06 17:34:00 +0200

    Minor fix

--- PainterApp/models.py
+++ PainterApp/models.py
@@ -8,7 +8,6 @@ class Block(models.Model):
 
 
 class MasterPiece(models.Model):
-    MasterPiece = models.ImageField(upload_to='Masterpieces')
+    MasterPiece = models.ImageField(upload_to="Masterpieces")
     Name = models.CharField(max_length=300, null=True, blank=True)
     Block = models.ForeignKey(Block, on_delete=models.CASCADE, null=True, blank=True)
-
